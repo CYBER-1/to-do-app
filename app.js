392da2b95397c94ca8e89591d9cf1d0617634d7d
Mykola Mykhailian <mnykolay@gmail.com>
2019-01-31 08:04:17 -0800

    final commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ function onReady() {
     const newToDoText = document.getElementById('newToDoText');
     const toDoList = document.getElementById('toDoList');
 
-    addToDoForm.addEventListener('submit', event => {
+    addToDoForm.addEventListener('submit', (event) => {
         event.preventDefault();
 
         // get the text
@@ -11,39 +11,32 @@ function onReady() {
 
         // create a new li
         let newLi = document.createElement('li');
-
         // create a new input
         let checkbox = document.createElement('input');
-
         // set the input's type to checkbox
         checkbox.type = "checkbox";
 
         //create delete button
         let deleteButton = document.createElement('button');
+        deleteButton.textContent = "Delete";
 
-        //create a text node
-        let = document.createTextNode("delete")
+        //add delete event listener
+        deleteButton.addEventListener('click', function(event) {
+        toDoList.removeChild(this.parentElement);
+        })
 
         // set the title
         newLi.textContent = title;
-
         // attach the checkbox to the li
         newLi.appendChild(checkbox);
-        
         //attach the delete button to the li
         newLi.appendChild(deleteButton);
-
-        //add delete event listener
-        deleteButton.addEventListener('click', () => {
-        toDoList.removeChild(newLi);
-
         // attach the li to the ul
         toDoList.appendChild(newLi);
-
         //empty the input
         newToDoText.value = '';
- });     
- }
+    });
+};
 
  window.onload = function() {
     onReady();
